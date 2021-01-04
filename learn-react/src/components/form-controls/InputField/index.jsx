be47f1c957a15c99d8dd11ebbb633e525fc732f0
CAVOINHO <kentnguyen101@gmail.com>
2021-01-04 14:16:33 +0700

    Form Control

--- learn-react/src/components/form-controls/InputField/index.jsx
+++ learn-react/src/components/form-controls/InputField/index.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { TextField } from '@material-ui/core';
+import { Controller } from 'react-hook-form';
 
 InputField.propTypes = {
-    
+    form: PropTypes.object.isRequired, //quan trong
+    name: PropTypes.string.isRequired, //quan trong
+
+    label: PropTypes.string,
+    disabled: PropTypes.bool,
 };
 
 function InputField(props) {
+    const {form, name, label, disabled} = props;
+
     return (
-        <div>
-            <TextField fullWidth/>
-        </div>
+        <Controller
+            name={name} //bat buoc
+            control={form.control} //bat buoc
+            as={TextField}
+
+            fullWidth
+            label={label}
+            disabled={disabled}
+        >
+        
+        </Controller>
     );
 }
 
--- learn-react/src/features/Todo/Components/TodoForm/index.jsx
+++ learn-react/src/features/Todo/Components/TodoForm/index.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import InputField from '../../../../components/form-controls/InputField';
+import { useForm } from 'react-hook-form';
 
 TodoForm.propTypes = {
     onSubmit: PropTypes.func,
 };
 
 function TodoForm(props) {
+    const form = useForm({
+        defaultValues: {
+            title: '',
+        },
+    });
+
+    const handleSubmit = (values) => {
+        console.log('TODO FORM: ', values);
+    }
+
     return (
-        <div>
-            Todo Form
-        </div>
+        <form onSubmit={form.handleSubmit(handleSubmit)}>
+            <InputField name="title" title="Todo" form={form}/>
+        </form>
     );
 }
 

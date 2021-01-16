b155c41eb1818d9341300529ea8f8feba949868f
CAVOINHO <kentnguyen101@gmail.com>
2021-01-17 00:09:08 +0700

    logout user

--- learn-react/src/components/Header/index.jsx
+++ learn-react/src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@material-ui/core';
+import { Box, IconButton, Menu, MenuItem } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -6,10 +6,12 @@ import DialogContent from '@material-ui/core/DialogContent';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { Close } from '@material-ui/icons';
+import { AccountCircle, Close } from '@material-ui/icons';
 import CodeIcon from '@material-ui/icons/Code';
 import Login from 'features/Auth/components/Login';
+import { logout } from 'features/Auth/userSlice';
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, NavLink } from 'react-router-dom';
 import Register from '../../features/Auth/components/Register';
 
@@ -43,7 +45,7 @@ const useStyles = makeStyles((theme) => ({
     zIndex: 1,
     transform: 'translateZ(0px)',
     minWidth: '36px',
-  }
+  },
 }));
 
 const MODE = {
@@ -52,8 +54,12 @@ const MODE = {
 };
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const loggedInUser = useSelector((state) => state.user.current);
+  const isLoggedIn = !!loggedInUser.id;
   const [open, setOpen] = useState(false);
   const [mode, setMode] = useState(MODE.LOGIN);
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -61,6 +67,19 @@ export default function Header() {
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const handleUserClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogoutClick = () => {
+    const action = logout();
+    dispatch(action);
   };
 
   const classes = useStyles();
@@ -84,11 +103,38 @@ export default function Header() {
             <Button color="inherit">Albums</Button>
           </NavLink>
 
-          <Button className={classes.link} onClick={handleClickOpen} color="inherit">
-            Login
-          </Button>
+          {!isLoggedIn && (
+            <Button className={classes.link} onClick={handleClickOpen} color="inherit">
+              Login
+            </Button>
+          )}
+
+          {isLoggedIn && (
+            <IconButton color="inherit" onClick={handleUserClick}>
+              <AccountCircle />
+            </IconButton>
+          )}
         </Toolbar>
       </AppBar>
+
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleCloseMenu}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        getContentAnchorEl={null}
+      >
+        <MenuItem onClick={handleCloseMenu}>My account</MenuItem>
+        <MenuItem onClick={handleLogoutClick}>Logout</MenuItem>
+      </Menu>
 
       <Dialog
         disableBackdropClick
@@ -98,29 +144,29 @@ export default function Header() {
         aria-labelledby="form-dialog-title"
         maxWidth="xs"
       >
-        <Button className={ classes.closeButton } onClick={handleClose}>
+        <Button className={classes.closeButton} onClick={handleClose}>
           <Close />
         </Button>
 
-        <DialogContent >
-          { mode === MODE.REGISTER && (
+        <DialogContent>
+          {mode === MODE.REGISTER && (
             <>
-              <Register closeDialog={ handleClose }/>
+              <Register closeDialog={handleClose} />
 
               <Box textAlign="center">
-                <Button color="primary" onClick={ () => setMode(MODE.LOGIN) }>
-                  Already have an account. Login here 
+                <Button color="primary" onClick={() => setMode(MODE.LOGIN)}>
+                  Already have an account. Login here
                 </Button>
               </Box>
             </>
           )}
 
-          { mode === MODE.LOGIN && (
+          {mode === MODE.LOGIN && (
             <>
-              <Login closeDialog={ handleClose }/>
+              <Login closeDialog={handleClose} />
 
               <Box textAlign="center">
-                <Button color="primary" onClick={ () => setMode(MODE.REGISTER) }>
+                <Button color="primary" onClick={() => setMode(MODE.REGISTER)}>
                   Don't have an account. Register here.
                 </Button>
               </Box>
